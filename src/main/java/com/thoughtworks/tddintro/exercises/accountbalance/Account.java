53f3c90c5482cd818ee83c1622766ff3d645fd51
lingtran <lxiutran@gmail.com>
2016-12-20 15:20:40 -0600

    Pass shouldIncreaseMyBalanceWhenIDepositMoney unit test for AccountTests
    Create private instance var for balance along with getter/setter methods and deposit method
    Pass unit test with initial implementation

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,17 +3,27 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account();
+        account.setBalance(100);
+        Integer initialBalance = account.getBalance();
+
+        account.deposit(50);
+        Integer result = account.getBalance();
+
+        assertThat(result, is(150));
+        assertTrue(result > initialBalance);
     }
 
     @Test
     @Ignore  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
     }
 
     @Test
